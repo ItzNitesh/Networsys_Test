21ada655603a7025b152611192bddd1bf8549415
Nitesh Gupta <107179414+ItzNitesh@users.noreply.github.com>
2025-05-25 01:52:09 +0530

    Add files via upload

---
+++ asstes/js/script.js
@@ -0,0 +1,41 @@
+function openTab(evt, tabId) {
+    const contents = document.querySelectorAll('.tab-content');
+    contents.forEach(c => c.classList.remove('active'));
+
+
+    const buttons = document.querySelectorAll('.tab-button');
+    buttons.forEach(b => b.classList.remove('active'));
+
+    document.getElementById(tabId).classList.add('active');
+    evt.currentTarget.classList.add('active');
+}
+
+
+window.addEventListener('load', function () {
+    setTimeout(function () {
+        const myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
+        myModal.show();
+    }, 2000); 
+});
+
+
+
+
+document.getElementById("submitForm").addEventListener("click", function () {
+    const fields = ["f_name", "l_name", "phone", "email", "message"];
+    let allFilled = true;
+
+    fields.forEach(id => {
+        const field = document.getElementById(id);
+        if (!field.value.trim()) {
+            allFilled = false;
+        }
+    });
+
+
+    if (!allFilled) {
+        alert("Please Fill ")
+    } else {
+        alert("Form submitted successfully!");
+    }
+});
